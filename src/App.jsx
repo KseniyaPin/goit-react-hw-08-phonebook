44a85249c7c4ab5392a864663a8c82f8dfdf4090
KseniyaPin <kontr.mat@gmail.com>
2023-09-27 19:46:53 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import AppBar from './components/AppBar/App.bar';
 import { getUser } from './components/redux/selectors';
 
-// import HomeView from './components/views/HomeView';
+import HomeView from './components/views/HomeView';
 import RegisterView from './components/views/RegisterView';
 import LoginView from './components/views/LoginView';
 import {ContactsView} from './components/views/ContactsView';
@@ -19,7 +19,7 @@ export default function App() {
         <Route path="/login" element={user?.token ? <Navigate to="/contacts" /> : <LoginView />} />
         <Route path="/register" element={user?.token ? <Navigate to="/contacts" /> : <RegisterView />} />
         <Route path="/contacts" element={!user?.token ? <Navigate to="/login" /> : <ContactsView />} />
-        <Route path="*" element={ user?.token ? ( <Navigate to="/contacts" />  ) : ( <Navigate to="/login" /> )} />
+        <Route path="*" element={ user?.token ? ( <Navigate to="/contacts" />  ) : <HomeView /> } />
 
 {/*           
           <Route path='/' element={ <LoginView /> } />
